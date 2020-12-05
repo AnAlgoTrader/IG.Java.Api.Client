6469b9aa4391179ee3a026ef11025a7b83479ef3
oneangrytrader <one.angry.trader@gmail.com>
2020-12-05 19:11:25 +0000

    .

--- src/main/java/ig/api/client/rest/RestClient.java
+++ src/main/java/ig/api/client/rest/RestClient.java
@@ -1,37 +1,45 @@
 package ig.api.client.rest;
 
+import ig.api.client.rest.request.AuthenticationRequest;
+import ig.api.client.rest.response.AuthenticationResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 public class RestClient {
 
-    private String baseUri;
+    private final String baseUri;
+    private final String SESSION_URI = "/gateway/deal/session";
+    private String key;
+    private String token;
+    private String clientId;
 
     public RestClient(String environment) {
-
+        baseUri = String.format("https://%sapi.ig.com", "live".equals(environment) ? "" : "demo-");
     }
 
     public void Authenticate(String username, String password, String apiKey) throws IOException, InterruptedException {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
-            HttpPost httpPost = new HttpPost(baseUri);
-            String json = "{\"id\":1,\"name\":\"John\"}";
-            StringEntity entity = new StringEntity(json);
+            HttpPost httpPost = new HttpPost(baseUri + SESSION_URI);
+            AuthenticationRequest request = new AuthenticationRequest();
+            request.setIdentifier(username);
+            request.setPassword(password);
+            StringEntity entity = new StringEntity(AuthenticationRequest.toJsonString(request));
             httpPost.setEntity(entity);
             httpPost.setHeader("Accept", "application/json");
             httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("Version", "3");
+            httpPost.setHeader("X-IG-API-KEY", apiKey);
             try (CloseableHttpResponse response = client.execute(httpPost)) {
-                System.out.println("----------------------------------------");
-                System.out.println(response.getStatusLine());
-                System.out.println(EntityUtils.toString(response.getEntity()));
+                String responseString = new BasicResponseHandler().handleResponse(response);
+                AuthenticationResponse authenticationResponse = AuthenticationResponse.fromJsonString(responseString);
+                key = apiKey;
+                token = authenticationResponse.getOauthToken().getAccessToken();
+                clientId = authenticationResponse.getClientID();                
             }
         }
     }
