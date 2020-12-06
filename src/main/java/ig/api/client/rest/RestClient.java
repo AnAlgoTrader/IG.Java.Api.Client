4e17d7f3e0a1df8a48b1e56f860bc7b251961cbe
oneangrytrader <one.angry.trader@gmail.com>
2020-12-06 14:51:19 +0000

    .

--- src/main/java/ig/api/client/rest/RestClient.java
+++ src/main/java/ig/api/client/rest/RestClient.java
@@ -6,19 +6,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class RestClient {
 
     private final String baseUri;
     private final String SESSION_URI = "/gateway/deal/session";    
+    private final String ACCOUNTS_URI = "/gateway/deal/accounts";    
     private final CloseableHttpClient closeableHttpClient;
     private AuthenticationResponse authenticationResponse;
+    private String key;
 
     public RestClient(String environment) {
         baseUri = String.format("https://%sapi.ig.com", "live".equals(environment) ? "" : "demo-");
@@ -42,9 +48,16 @@ public class RestClient {
         final int MILLI_TO_HOUR = 1000 * 60 * 60;
         return Math.abs((int) (end.getTime() - start.getTime()) / MILLI_TO_HOUR);
     }
+    
+    public void GetAccounts() throws IOException{
+        String response = Get(baseUri + ACCOUNTS_URI, "1");
+        System.out.println(response);        
+    }
 
     public AuthenticationResponse Authenticate(String username, String password, String apiKey, String authResponsePath) throws IOException, InterruptedException {
 
+        key = apiKey;
+        
         if (ShouldAuthenticate(authResponsePath)) {
 
             HttpPost httpPost = new HttpPost(baseUri + SESSION_URI);
@@ -53,8 +66,8 @@ public class RestClient {
             request.setPassword(password);
             StringEntity entity = new StringEntity(AuthenticationRequest.toJsonString(request));
             httpPost.setEntity(entity);
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader(HttpHeaders.ACCEPT, "application/json");
+            httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
             httpPost.setHeader("Version", "3");
             httpPost.setHeader("X-IG-API-KEY", apiKey);
             try (CloseableHttpResponse response = closeableHttpClient.execute(httpPost)) {
@@ -67,6 +80,20 @@ public class RestClient {
         }
 
         return authenticationResponse;
+    }
+    
+    public String Get(String url, String version) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader(HttpHeaders.ACCEPT, "application/json");
+        httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");        
+        httpGet.setHeader("VERSION", version);
+        httpGet.setHeader("X-IG-API-KEY", key);
+        httpGet.setHeader("IG-ACCOUNT-ID", authenticationResponse.getAccountID());
+        httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authenticationResponse.getOauthToken().getAccessToken());
+        try (CloseableHttpResponse response = closeableHttpClient.execute(httpGet)) {
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity);
+        }
     }
     
     public void Close() throws IOException{
